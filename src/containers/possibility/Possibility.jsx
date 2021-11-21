44c9690d58e2e58d02b9ae2f6a3db7773dcf1d44
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2021-11-21 14:53:56 +0000

    Finished cta and possibility components

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -3,9 +3,18 @@ import './possibility.css';
 
 const Possibility = () => {
     return (
-        <div>
-            Possibility
-        </div>
+        <section className='gpt3__possibility section__padding' id='possibility'>
+            <div className="gpt3__possibility-image">
+                <img src="images/possibility.png" alt="possibility" />
+            </div>
+
+            <div className="gpt3__possibility-content">
+                <h4>Request Early Access to Get Started</h4>
+                <h2 className="gradient__text">The possibilities are beyond your imagination</h2>
+                <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+                <h4>Request Early Access to Get Started</h4>
+            </div>
+        </section>
     )
 }
 
